a4d8335d5d07f03c372959bbd13e8fe1290f0af5
Djavan Cackley <djavancackley@Djavans-MBP.home>
2019-03-06 20:55:53 -1000

    completed

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -1,9 +1,21 @@
+DROP DATABASE IF EXISTS denormal_cars;
+DROP USER IF EXISTS denormal_user;
 CREATE USER denormal_user;
-CREATE DATABASE denormal_cars;
-OWNER denormal_user;
+CREATE DATABASE denormal_cars WITH OWNER denormal_user;
+--\du
+--\l
+\c denormal_cars;
 \i scripts/denormal_data.sql;
+\dS car_models;
 
-SELECT DISTINCT make_title from car_models;
-SELECT DISTINCT model_title from car_models WHERE make_code='VOLKS';
-SELECT DISTINCT make_code='LAM';
-SELECT COUNT (*) FROM car_models WHERE YEAR >= 2010 AND YEAR <=2015;
+SELECT DISTINCT make_title FROM car_models;
+
+SELECT DISTINCT model_title FROM car_models WHERE make_code ='VOLKS';
+
+SELECT DISTINCT make_code, model_code, model_title, year
+FROM car_models
+WHERE make_code = 'LAM';
+
+SELECT COUNT (*)
+FROM car_models 
+WHERE YEAR >= 2010 AND YEAR <=2015;
--- normal_cars.sql
+++ normal_cars.sql
@@ -1,45 +1,61 @@
-DROP USER IF EXISTS normal_user;
 DROP DATABASE IF EXISTS normal_cars;
-DROP TABLE IF EXISTS make;
-DROP TABLE IF EXISTS model;
-DROP TABLE IF EXISTS year_TABLE;
+DROP USER IF EXISTS normal_user;
 CREATE USER normal_user;
 CREATE DATABASE normal_cars WITH OWNER normal_user;
+DROP TABLE IF EXISTS make;
+DROP TABLE IF EXISTS model;
+DROP TABLE IF EXISTS yearTable;
 
 \i scripts/denormal_data.sql;
+\c normal_cars; 
 
 CREATE TABLE make (
-    ID SERIAL NOT NULL PRIMARY KEY,
-    make_code character varying(125) NOT NULL,
-    make_title character varying(125) NOT NULL
+    ID SERIAL PRIMARY KEY,
+    make_code VARCHAR(25) NOT NULL,
+    make_title VARCHAR(25) NOT NULL
 );
 
-INSERT INTO make(make_code, make_title)
+INSERT INTO make (make_code, make_title)
     SELECT DISTINCT make_code, make_title
-        FROM car_models
-        ORDER BY make_code;
+    FROM car_models
+    ORDER BY make_code;
+
+ -- SELECT * FROM make;
+    -- makes a table model with model_code and model_title from car_models and makes alphabetically organized
 
 CREATE TABLE model (
-    ID SERIAL NOT NULL PRIMARY KEY,
-    model_code character varying(125) NOT NULL;
-    model_title character varying(125) NOT NULL
+    ID SERIAL PRIMARY KEY,
+    model_code VARCHAR(125) NOT NULL,
+    model_title VARCHAR(125) NOT NULL
 );
 
-INSERT INTO model(model_code, model_title)
+INSERT INTO model (model_code, model_title)
     SELECT DISTINCT model_code, model_title
     FROM car_models
     ORDER BY model_code;
 
-CREATE TABLE year_TABLE(
-    ID SERIAL NOT NULL PRIMARY KEY,
+CREATE TABLE yearTable (
+    ID SERIAL PRIMARY KEY,
     year INTEGER NOT NULL
 );
 
-INSERT INTO year_TABLE(year)
+INSERT INTO yearTable (year)
     SELECT DISTINCT year
     FROM car_models
     ORDER BY year;
 
-SELECT COUNT(*) FROM car_models
-WHERE year
-BETWEEN 2010 AND 2015;
+
+SELECT DISTINCT make_code, model_title
+    FROM car_models
+    WHERE make_code = 'VOLKS';
+
+SELECT DISTINCT make_code, model_code, model_title, year
+    FROM car_models
+    WHERE make_code = 'LAM';
+
+SELECT DISTINCT COUNT(*) 
+    FROM car_models
+    WHERE year
+    BETWEEN 2010 AND 2015;
+
+-- -- \i scripts/denormal_data.sql;
